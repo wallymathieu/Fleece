b3be445440187a404817a091ba023bff2bf6f19f
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-12-27 08:36:32 +0100

    Some sample code

--- docsrc/content/codec.fsx
+++ docsrc/content/codec.fsx
@@ -120,3 +120,86 @@ let personBytesCodec2 =
     |> Codec.compose jsonObjToValueCodec    // this is the codec that fills the gap to/from JsonValue
     |> Codec.compose jsonValueToTextCodec   // this is a codec between JsonValue and JsonText
     |> Codec.invmap getString Encoding.UTF8.GetBytes    // This is a pair of of isomorphic functions
+
+
+open System.Json
+open Fleece.SystemJson
+open Fleece.SystemJson.Operators
+open System
+open FSharpPlus
+type Auction={id:string; at:DateTime option}
+with
+  static member JsonObjCodec1_0 =
+    fun id -> { id =id; at = None }
+    |> withFields
+    |> jfield "id"      (fun x -> x.id)
+    |> Codec.compose jsonObjToValueCodec
+  static member JsonObjCodec1_1 =
+    fun id at -> { id =id; at = at }
+    |> withFields
+    |> jfield "id"      (fun x -> x.id)
+    |> jfield "at"      (fun x -> x.at)
+    |> Codec.compose jsonObjToValueCodec
+
+type Bid={auction:string}
+with
+  static member JsonObjCodec =
+    fun auction -> { auction =auction; }
+    |> withFields
+    |> jfield "auction"      (fun x -> x.auction)
+
+type Payload =
+  | AuctionAdded of DateTime * Auction
+  | BidPlaced of DateTime * Bid
+
+  /// the time when the payload was issued
+  static member getAt payload =
+    match payload with
+    | AuctionAdded(at, _) -> at
+    | BidPlaced(at, _) -> at
+
+  static member getAuction payload =
+    match payload with
+    | AuctionAdded(_,auction) -> auction.id
+    | BidPlaced(_,bid) -> bid.auction
+  static member OfJson_base json auctionCodecOfJson =
+    match json with
+    | JObject o -> monad {
+        let! t = o .@ "$type"
+        match t with
+        | "AuctionAdded" ->
+          let create d a = AuctionAdded (d,a)
+          return! (create <!> (o .@ "at") <*> (jgetWith auctionCodecOfJson o "auction"))
+        | "BidPlaced" ->
+          let create d b = BidPlaced (d,b)
+          return! (create <!> (o .@ "at") <*> (o .@ "bid"))
+        | x -> return! (Decode.Fail.invalidValue json (sprintf "Expected one of: 'AddAuction', 'PlaceBid' for $type but %s" x))
+      }
+    | x -> Decode.Fail.objExpected json
+  
+  static member OfJson1_0 json =
+    Payload.OfJson_base json (fst Auction.JsonObjCodec1_0)
+  static member OfJson1_1 json =
+    Payload.OfJson_base json (fst Auction.JsonObjCodec1_1)
+
+  static member ToJson_base (x: Payload) auctionToJson =
+    match x with
+    | AuctionAdded (d,a)-> jobj [ "$type" .= "AuctionAdded"; "at" .= d; (jpairWith auctionToJson "auction" a)]
+    | BidPlaced (d,b)-> jobj [ "$type" .= "BidPlaced"; "at" .= d; "bid" .= b]
+
+  static member ToJson1_0 (x: Payload) =
+    Payload.ToJson_base x (snd Auction.JsonObjCodec1_0)
+  static member ToJson1_1 (x: Payload) =
+    Payload.ToJson_base x (snd Auction.JsonObjCodec1_1)
+
+type StreamId = StreamId of string
+type VersionNumber = VersionNumber of string
+type EventWrite<'payload, 'metadata> =
+  { Id: Guid                     // ID of the event
+    StreamId: StreamId           // Id of the stream this event belongs to
+    VersionNumber: VersionNumber // Sequence number of the event in a single stream
+    SchemaRevision: int          // Schema revision of the payload and metadata
+    EventType: string            // Event Type
+    Payload: 'payload            // Event data
+    Timestamp: DateTime }        // Timestamp the event was generated
+
